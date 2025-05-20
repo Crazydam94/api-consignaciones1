2f2381551497fdecc377a0840aa28b1ef6e34263
Crazydam94 <salv_1994@hotmail.com>
2025-05-20 15:51:10 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<ConsignacionesApi.Services.ConexionBD>();
 
 var app = builder.Build();
 
